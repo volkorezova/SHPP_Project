4d37af8c5092edb0009632de6d5f1d855d582a8d
Tatyana Volkorezova <tatyana.volkorezova@onix-systems.com>
2017-10-03 22:59:40 +0300

    rrrr

---
+++ SHPP_Volkorezova/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SHPP_Volkorezova
+//
+//  Created by Tatyana Volkorezova on 03.10.2017.
+//  Copyright © 2017 shpp.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
