620fe25b103e7a6abea5d1ac32d60449ec07c386
Tatyana Volkorezova <tatyana.volkorezova@onix-systems.com>
2017-10-07 21:53:05 +0300

    Home Task#1. Implementation of Christmas tree and two triangls (from max to min, from min to max)

--- SHPP_Volkorezova/main.c
+++ SHPP_Volkorezova/main.c
@@ -7,10 +7,50 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n\n");
+int main() {
+    
+    int height;
+    printf("Please enter height of Christmas tree: ");
+    scanf("%d", &height);
+    
+    if ((height<=0)||(height>9)){
+        printf("Sorry but you've entered incorrect value of tree height. Pls enter digit from 1-9 \n");
+        exit(0);
+    }
+    //implementation of Christmas tree
+    for(int i = 1; i <= height; i++) {
+        for(int j = 2; j <= (height+i); j++){
+            if(j <= (height - i+1))
+                printf(" ");
+            else
+                printf("*");
+        }
+       printf("\n");
+    }
+    //implementation of stump for tree (penek)
+    for(int j = 1; j <= height-1; j++){
+        printf(" ");
+    }
+    printf("*");
+    
+    printf("\n");
+   //half of triangle (from min to max)
+    for(int i = 0; i <=height; i++){
+        for (int j = 0; j <= i-1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+   printf("\n");
+   //half of triangle (from max to min)
+    for(int i = 0; i <height; i++) {
+        for(int j = 0; j < height-i; j++)
+            printf("*");
+        printf("\n");
+    }
+
     return 0;
 }
-
+    
